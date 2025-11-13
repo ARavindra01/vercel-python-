50c44f6cb9e7d06cd0441dfe646afa7b7dda9c21
ARavindra01 <ravipro079@gmail.com>
2025-11-13 22:58:43 +0530

    My first website

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify
+
+# Vercel will run this 'app' object
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def handle_greet():
+    # This function will run when someone visits /api/greet
+    return jsonify(message="Hello from the Python (Flask) API!")
