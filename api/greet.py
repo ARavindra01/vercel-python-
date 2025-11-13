707d8bee65ccaf4a44d1403d06ae7e4f2d81e2f9
ARavindra01 <ravipro079@gmail.com>
2025-11-13 23:20:09 +0530

    Fix: Corrected Python API route

--- api/greet.py
+++ api/greet.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 # Vercel will run this 'app' object
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api/greet', methods=['GET'])
 def handle_greet():
     # This function will run when someone visits /api/greet
     return jsonify(message="Hello from the Python (Flask) API!")
